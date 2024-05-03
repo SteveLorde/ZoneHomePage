ab01678fbfb0de9cb0424567c3584d72fc6b3c31
SteveLorde <mostafa.maher98@gmail.com>
2024-05-03 02:42:56 +0200

    project init, created folders, implemented component like design for the website

---
+++ js/web.js
@@ -0,0 +1,17 @@
+let indexNavbarElement = document.getElementById("index-navbar");
+let indexPageOutlet = document.getElementById("pageoutlet");
+
+
+
+function AppendNavbarComponent() {
+    fetch("partialviews/navbar.html").then(res => res.text()).then(navbarHTML => indexNavbarElement.innerHTML = navbarHTML)
+}
+
+
+function SelectPageComponent(number) {
+    if (number === 0) {
+        fetch("pages/home.html").then(res =>res.text()).then(PageHTML => indexPageOutlet.innerHTML = PageHTML)
+    } else if (number === 1) {
+        fetch("pages/features.html").then(res =>res.text()).then(PageHTML => indexPageOutlet.innerHTML = PageHTML)
+    }
+}
